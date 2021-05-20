b22977d776a9b2ea60ba110c04cf9a852e33c816
AUNTY-BETTY-pnppb <61230402+AUNTY-BETTY-pnppb@users.noreply.github.com>
2021-05-20 13:40:21 +0100

    added a couple more widgets
    more widgets for main play

--- ShubaPlay.py
+++ ShubaPlay.py
@@ -22,20 +22,24 @@ class ShubaPlay:
     def __init__(self, parent):
         self._parent = parent
         self.frame = tk.Frame(self._parent)
+        self._prog_var = tk.IntVar()
         self._canvas = tk.Canvas(self.frame, bg="blue", height=250, width=300)
-        self._prevlist = tk.Listbox(self.frame, height=20, width=30)
-        self._tracklist = tk.Listbox(self.frame, height=20, width=30)
+        self._buffer = ttk.Progressbar(self.frame, orient="horizontal", length=280, maximum=100, mode="determinate", var=self._prog_var)
+        self._prevlist = tk.Listbox(self.frame, bg="green", height=5, width=30)
+        self._tracklist = tk.Listbox(self.frame, bg="red", height=20, width=30)
         self._playbtn = tk.Button(self.frame, text='Play', padx=25, pady=15)
         self._prevbtn = tk.Button(self.frame, text='Prev', padx=25, pady=15)
         self._nextbtn = tk.Button(self.frame, text='Next', padx=25, pady=15)
         self.pos_widgets()
 
     def pos_widgets(self):
-        self._canvas.grid(row=1, column=1, columnspan=3)
+        self._canvas.grid(row=0, column=1, columnspan=3, rowspan=2)
+        self._buffer.grid(row=2, column=1, columnspan=3)
+        self._prevlist.grid(row=0, column=4)
         self._tracklist.grid(row=1, column=4, rowspan=3)
-        self._prevbtn.grid(row=2, column=1)
-        self._playbtn.grid(row=2, column=2)
-        self._nextbtn.grid(row=2, column=3)
+        self._prevbtn.grid(row=3, column=1, sticky="n")
+        self._playbtn.grid(row=3, column=2, sticky="n")
+        self._nextbtn.grid(row=3, column=3, sticky="n")
 
 class Playlist:
 

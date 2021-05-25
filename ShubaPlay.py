515cf9bc463eae93cac9696b4d6b783396049d2a
AUNTY-BETTY-pnppb <61230402+AUNTY-BETTY-pnppb@users.noreply.github.com>
2021-05-25 13:05:31 +0100

    Update with music ADT
    added the music functions

--- ShubaPlay.py
+++ ShubaPlay.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+import pygame 
+import glob 
+import time 
+import os
 
 class Main:
 
@@ -17,29 +21,157 @@ class Main:
         self._tab_bar.add(self.playlist.frame, text='Playlist')
         self._tab_bar.grid(column=0, row=0)
 
+class musicADT:
+    def __init__(self):
+        types = ('./**/*.wav', './**/*.mp3')
+        self._files = []
+        self._current = 0
+        self.checkpause = False
+        self.start = False
+        self.re = 0
+
+        for files in types:
+            self._files.extend(glob.glob(files, recursive=True))
+
+        pygame.init()
+        # edit quality
+        pygame.mixer.pre_init(frequency=48000, size=32, buffer=1024)
+        pygame.mixer.init()
+    
+    def play(self):
+        if self.check():
+            pygame.mixer.music.stop()
+        pygame.mixer.music.load(self._files[self._current])
+        pygame.mixer.music.play()
+        print("Playing - " + os.path.basename(self._files[self._current])[:-4])
+        return os.path.basename(self._files[self._current])[:-4]
+
+    def pause(self):
+        if not self.checkpause:
+            pygame.mixer.music.pause()
+            self.checkpause = True
+        else:
+            pygame.mixer.music.unpause()
+            self.checkpause = False
+    
+    def stop(self):
+        if self.check():
+            pygame.mixer.music.stop()
+
+    def check(self):
+        if pygame.mixer.music.get_busy():
+            return True
+        else:
+            return False
+
+    def next_track(self):
+        if self._current == len(self._files)-1:
+            self._current = 0
+        else:
+            self._current += 1
+        return self.play()
+
+    def prev_track(self):
+        if self._current == 0:
+            self._current = len(self._files)-1
+        else:
+            self._current -=1
+        return self.play()
+
+    def rewind(self):
+        pygame.mixer.music.rewind()
+
+    def printList(self):
+        for file in self._files:
+            if file == self._files[self._current]:
+                print("-" + os.path.basename(self._files[file])[:-4])
+            else:
+                print(os.path.basename(self._files[file])[:-4])
+
+    def list_all(self):
+        bef, aft = self._files[:self._current], self._files[self._current+1:] 
+        for b in bef:
+            bef[bef.index(b)] = os.path.basename(b)[:-4]
+        for a in aft:
+            aft[aft.index(a)] = os.path.basename(a)[:-4]
+        return bef, aft
+
 class ShubaPlay:
 
     def __init__(self, parent):
         self._parent = parent
         self.frame = tk.Frame(self._parent)
+        self.style = ttk.Style(self._parent)
         self._prog_var = tk.IntVar()
-        self._canvas = tk.Canvas(self.frame, bg="blue", height=250, width=300)
-        self._buffer = ttk.Progressbar(self.frame, orient="horizontal", length=280, maximum=100, mode="determinate", var=self._prog_var)
-        self._prevlist = tk.Listbox(self.frame, bg="green", height=5, width=30)
-        self._tracklist = tk.Listbox(self.frame, bg="red", height=20, width=30)
-        self._playbtn = tk.Button(self.frame, text='Play', padx=25, pady=15)
-        self._prevbtn = tk.Button(self.frame, text='Prev', padx=25, pady=15)
-        self._nextbtn = tk.Button(self.frame, text='Next', padx=25, pady=15)
+        self._canvas = tk.Canvas(self.frame, bg="white", height=250, width=300)
+        self._label = tk.Label(self.frame, text="")
+        self._prevlist = tk.Listbox(self.frame, height=5, width=30)
+        self._tracklist = tk.Listbox(self.frame, height=20, width=30)
+        self._prevlist.insert(0, "Previous")
+        self._tracklist.insert(0, "Next")
+        self._playbtn = tk.Button(self.frame, text='Play', padx=15, pady=15, command=self.play)
+        self._prevbtn = tk.Button(self.frame, text='Prev', padx=15, pady=15, command=self.prev)
+        self._nextbtn = tk.Button(self.frame, text='Next', padx=15, pady=15, command=self.next)
         self.pos_widgets()
 
     def pos_widgets(self):
-        self._canvas.grid(row=0, column=1, columnspan=3, rowspan=2)
-        self._buffer.grid(row=2, column=1, columnspan=3)
-        self._prevlist.grid(row=0, column=4)
-        self._tracklist.grid(row=1, column=4, rowspan=3)
-        self._prevbtn.grid(row=3, column=1, sticky="n")
-        self._playbtn.grid(row=3, column=2, sticky="n")
-        self._nextbtn.grid(row=3, column=3, sticky="n")
+        self._canvas.grid(row=0, column=1, columnspan=7, rowspan=2)
+        self._label.grid(row=2, column=1, columnspan=7)
+        self._prevlist.grid(row=0, column=8)
+        self._tracklist.grid(row=1, column=8, rowspan=3)
+        self._prevbtn.grid(row=3, column=3, sticky="n")
+        self._playbtn.grid(row=3, column=4, sticky="n")
+        self._nextbtn.grid(row=3, column=5, sticky="n")
+
+    def insert_list(self, bef, aft):
+        b = 1
+        a = 1
+        if len(bef) != 0:
+            if len(bef) > 4:
+                bef = bef[-4:]
+
+            for song in bef:
+                self._prevlist.insert(b, song)
+                b+=1
+
+        if len(aft) != 0:
+            for song in aft:
+                self._tracklist.insert(a, song)
+                a+=1
+    
+    def reset_box(self):
+        self._prevlist.delete(1,'end')
+        self._tracklist.delete(1, 'end')
+
+    def play(self):
+        if not music.check() and music.start == False:
+            l = music.play()
+            self.reset_box()
+            bef, aft = music.list_all()
+            self.insert_list(bef, aft)
+            self._label['text'] = l
+            music.start = True
+        else:
+            music.pause()    
+
+    def prev(self):
+        if music.re == 0 and music.check():
+            music.rewind()
+            music.re = 1
+        else:
+            l = music.prev_track()
+            self.reset_box()
+            bef, aft = music.list_all()
+            self.insert_list(bef, aft)
+            self._label['text'] = l
+            music.re = 0
+
+    def next(self):
+        l = music.next_track()
+        self.reset_box()
+        bef, aft = music.list_all()
+        self.insert_list(bef, aft)
+        self._label['text'] = l
 
 class Playlist:
 
@@ -47,5 +179,6 @@ class Playlist:
         self._parent = parent
         self.frame = tk.Frame(self._parent)
 
+music = musicADT()
 app = Main()
 app.root.mainloop()
